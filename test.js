f2bacaa4a7c0844d9fb46bb9af33dfba2fc37a58
prachi <prachidhage3@gmail.com>
2023-07-16 17:11:12 +0000

    3 rd commit node

---
+++ test.js
@@ -0,0 +1,6 @@
+var request = require('supertest');
+var app = require('.../index.js');describe('GET /', function () {
+it('respond with hello world', function(done) {
+request(app).get('/').expect('hello joker', done);
+});
+});
